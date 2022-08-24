83396478332fe0020da3a2f5a2ed609323f453c8
Nick <nick.stepanenko@trilogy.com>
2022-08-24 16:25:02 +0300

    feat: new feature added

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          {/* https://grotesque-watch.surge.sh/  */}
+          Learn React 
+          {/* https://grotesque-watch.surge.sh/ */}
         </a>
       </header>
     </div>
