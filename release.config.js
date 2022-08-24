7e4e88cb81304b059ccc019cdd7c4b10e1af4eff
Nick <nick.stepanenko@trilogy.com>
2022-08-24 20:56:39 +0300

    Revert "zip assets"
    This reverts commit 19064e2e499e1489724dda99cbe425c3df0fea25.

--- release.config.js
+++ release.config.js
@@ -4,20 +4,6 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          {
-            path: "build.zip",
-            label: "build"
-          },
-          {
-            path: "coverage.zip",
-            label: "coverage"
-          }
-        ]
-      }
-    ]
+    "@semantic-release/github"
   ]
 };
