7019db7d8fc37df9931d56d02863b9b23364930a
Nick <nick.stepanenko@trilogy.com>
2022-08-24 16:24:16 +0300

    semantic-release added

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/StepanenkoNS/section5",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
